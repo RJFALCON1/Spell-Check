6f08a39023d6d69d88b78df100fc7d03537ff288
RJFALCON1 <45921815+RJFALCON1@users.noreply.github.com>
2022-11-13 20:53:03 -0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from textblob import TextBlob
+def correct():
+    input=word1.get()
+    obj=TextBlob(input)
+    correctedWord=obj.correct()
+    word2.delete(0,END)
+    word2.insert(10,correctedWord)
+def clear():
+    word1.delete(0,END)
+    word2.delete(0,END)
+if __name__ == "__main__":
+    root=Tk()
+    root.configure(background="lightgreen")
+    root.geometry("500x300")
+    root.title("Spell Check")
+    header=Label(root,text="Spell Check.",fg="black",bg="white")
+    header.grid(row=0,column=1)
+    l1=Label(root,text="Input Word:",fg="black",bg="white")
+    l1.grid(row=1,column=1,pady=10)
+    word1=Entry()
+    word1.grid(row=1,column=2)
+    l2=Label(root,text="Corrected Word:",fg="black",bg="white")
+    l2.grid(row=3,column=1,pady=10)
+    word2=Entry()
+    word2.grid(row=3,column=2,padx=10)
+    button1=Button(root, text="Correct",bg="white",fg="black",command=correct)
+    button1.grid(row=2,column=2,pady=10)
+    button2=Button(root, text="Clear", bg="white",fg="black",command=clear)
+    button2.grid(row=4,column=2,pady=10)
+    root.mainloop()
